62402c7caf96afdb09040e7ebea16909d5012523
Gonzalo <gon.pineiro@gmail.com>
2021-09-07 09:42:01 -0300

    :recycle: Refactor del codigo

--- src/screens/Formulario/index.jsx
+++ src/screens/Formulario/index.jsx
@@ -3,8 +3,9 @@ import { connect } from "react-redux";
 
 import Element from "../../components/Element";
 import { FormContext } from "../FormContext";
+import { getFormDataByJson } from "./getForms";
+
 import {
-  getForm,
   className,
   emailIsValid,
   replaceUrl,
@@ -13,30 +14,6 @@ import {
 } from "../../utils";
 
 import "./index.scss";
-
-import JSONForm from "../../otro.json";
-
-// eslint-disable-next-line no-unused-vars
-const getFormData = async (setElements, setLoading, idForm) => {
-  const formData = await getForm(idForm);
-  if (!formData.error) {
-    const json = JSON.parse(formData.string);
-    json.fields = json.fields.sort((a, b) => a.field_order - b.field_order);
-    setElements(json);
-  } else {
-    setElements(undefined);
-  }
-  setLoading(false);
-};
-
-const getFormDataByJson = (setElements, setLoading, idForm) => {
-  const json = JSONForm[idForm];
-  if (json) {
-    json.fields = json.fields.sort((a, b) => a.field_order - b.field_order);
-    setElements(json);
-  }
-  setLoading(false);
-};
 
 const Formulario = ({ userReducer: { idForm } }) => {
   const [elements, setElements] = useState(null);
