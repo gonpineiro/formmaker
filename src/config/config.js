c232f3f085957c4e1229b74cebe92b213a1d4d6f
Gonzalo <gon.pineiro@gmail.com>
2021-09-10 13:49:24 -0300

    :config: Cambio en la configuracion

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,9 @@
+const PROD = process.env.REACT_APP_PROD === "true";
+
 export const URL_GET_TOKEN = process.env.REACT_APP_URL_GET_TOKEN;
-export const URL_BACK = process.env.REACT_APP_URL_BK;
+export const URL_BACK = PROD
+  ? process.env.REACT_APP_URL_BK_PROD
+  : process.env.REACT_APP_URL_BK_DEV;
 export const TOKEN = process.env.REACT_APP_TOKEN;
 export const APP_ID = parseInt(process.env.REACT_APP_APP_ID);
 export const ERROR_TOKEN = "Security Token incorrecto ó caducado";
