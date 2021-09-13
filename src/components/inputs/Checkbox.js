d037eda79f72a0fe97e121cf04bf9d2e52c849cf
Norbert Stange <n0rb3r7@outlook.com>
2021-09-13 14:48:47 -0300

    Actualización checkboxes.

--- src/components/inputs/Checkbox.js
+++ src/components/inputs/Checkbox.js
@@ -12,39 +12,6 @@ const Checkbox = ({
   const hanldeChecked = () => {
     setChecked(!checked);
   };
-  if (typeof field_options !== "undefined") {
-    return (
-      <>
-        <div className="mb-3">
-          <div className="card">
-            <div className="card-body">
-              <label className="form-check-label" htmlFor={field_id}>
-                {field_label + " sdfsad"}
-              </label>
-              {field_options.lenght > 0 &&
-                field_options.map((option, i) => (
-                  <div className="form-check" key={i}>
-                    <input
-                      type="checkbox"
-                      className="form-check-input"
-                      id={"id" + option.option_label}
-                      //name={field_name}
-                      value={option.option_label}
-                    />
-                    <label
-                      className="form-check-label"
-                      htmlFor={"id" + option.option_label}
-                    >
-                      {option.option_label}
-                    </label>
-                  </div>
-                ))}
-            </div>
-          </div>
-        </div>
-      </>
-    );
-  }
   return (
     <>
       <div className="mb-3 form-check">
@@ -57,7 +24,7 @@ const Checkbox = ({
           required={field_required}
         />
         <label className="form-check-label" htmlFor={field_id}>
-          {field_label + " ohohoh"}
+          {field_label + " sdohohoh"}
         </label>
       </div>
     </>
---
+++ src/components/inputs/Checkboxes.js
@@ -0,0 +1,42 @@
+const Checkboxes = ({
+  field_id,
+  field_label,
+  field_placeholder,
+  field_required,
+  field_value,
+  field_options,
+  field_name,
+}) => {
+  return (
+    <div className="mb-3">
+      <div className="card">
+        <div className="card-body">
+          <label className="form-check-label" htmlFor={field_id}>
+            {field_label}
+          </label>
+          <div className="container row">
+            {field_options.map((option, i) => (
+              <div className="form-check col-6" key={i}>
+                <input
+                  type="checkbox"
+                  className="form-check-input"
+                  id={"id" + option.option_label}
+                  name={field_name}
+                  value={option.option_label}
+                />
+                <label
+                  className="form-check-label"
+                  htmlFor={"id" + option.option_label}
+                >
+                  {option.option_label}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkboxes;
