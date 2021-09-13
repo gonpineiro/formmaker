f665f64bbebdd13ac2e8e88c49a62a6682d9f4e0
Gonzalo <gon.pineiro@gmail.com>
2021-09-13 10:21:07 -0300

    :hammer: Consulta de los formularios para generar un menu

--- backend/index.php
+++ backend/index.php
@@ -42,6 +42,7 @@ if (isset($_POST) &&  $_POST['token'] === TOKEN) {
             echo json_encode($error, true);
         }
     } */
+
     if (isset($_POST) && $_POST['type'] === 'get') {
         $formulario = $formController->getJson($_POST['id']);
         if ($formulario) {
@@ -51,6 +52,19 @@ if (isset($_POST) &&  $_POST['token'] === TOKEN) {
             $error = [
                 'error' =>  'Recurso no encontrado',
                 'idForm' => $_POST['id'],
+            ];
+            echo json_encode($error, true);
+        }
+    }
+
+    if (isset($_POST) && $_POST['type'] === 'get-all-form') {
+        $forms = $formController->getJsonForms();
+        if ($forms) {
+            $forms['error'] = null;
+            echo json_encode($forms, true);
+        } else {
+            $error = [
+                'error' =>  'Error a obtener los forms de formularios',
             ];
             echo json_encode($error, true);
         }
