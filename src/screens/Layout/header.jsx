963a5b7abb526e084d8d8f1ce090ecfbd9281071
Norbert Stange <n0rb3r7@outlook.com>
2021-09-15 13:14:28 -0300

    Link a root desde el logo del header.

--- src/screens/Layout/header.jsx
+++ src/screens/Layout/header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import "./header.scss";
 import LogoNeg from "./LogoNeg.js";
 
@@ -6,7 +8,12 @@ const Header = () => {
     <header>
       <div className="container">
         <div className="header-logo text-center">
-          <LogoNeg />
+          <Link
+            className="menu-link col-12 text-center"
+            to={"/apps/formmaker/"}
+          >
+            <LogoNeg />
+          </Link>
         </div>
       </div>
     </header>
