c7d713e7898c6f8b8d57c4b5f866e6f76bd5bcce
Gonzalo <gon.pineiro@gmail.com>
2021-09-16 10:22:34 -0300

    :hammer: Cambio configuracion del .env

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,16 @@
-const PROD = process.env.REACT_APP_PROD === "true";
+const URL_GET_TOKEN = process.env.REACT_APP_URL_GET_TOKEN;
+let URL_BACK;
+if (process.env.REACT_APP_ENV === "local") {
+  URL_BACK = process.env.REACT_APP_URL_BK_LOCAL;
+}
+if (process.env.REACT_APP_ENV === "replica") {
+  URL_BACK = process.env.REACT_APP_URL_BK_REPLICA;
+}
+if (process.env.REACT_APP_ENV === "produccion") {
+  URL_BACK = process.env.REACT_APP_URL_BK_PRODUCCION;
+}
+const TOKEN = process.env.REACT_APP_TOKEN;
+const APP_ID = parseInt(process.env.REACT_APP_APP_ID);
+const ERROR_TOKEN = "Security Token incorrecto ó caducado";
 
-export const URL_GET_TOKEN = process.env.REACT_APP_URL_GET_TOKEN;
-export const URL_BACK = PROD
-  ? process.env.REACT_APP_URL_BK_PROD
-  : process.env.REACT_APP_URL_BK_DEV;
-export const TOKEN = process.env.REACT_APP_TOKEN;
-export const APP_ID = parseInt(process.env.REACT_APP_APP_ID);
-export const ERROR_TOKEN = "Security Token incorrecto ó caducado";
+export { URL_GET_TOKEN, URL_BACK, TOKEN, APP_ID, ERROR_TOKEN };
