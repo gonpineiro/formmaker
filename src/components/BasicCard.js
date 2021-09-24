bb7434665f55628e797c8f16c5ab0272b7fcb0a1
Gonzalo <gon.pineiro@gmail.com>
2021-09-24 12:30:52 -0300

    :fix: Cambio de estilos en el Gestionar para que se no pisen

--- src/components/BasicCard.js
+++ src/components/BasicCard.js
@@ -2,7 +2,7 @@ const BasiCard = ({ title, description, id, handler }) => {
 
   return (
     <div className="col-12 col-md-4" onClick={() => handler(id)}>
-      <div className="card border-primary mb-3">
+      <div className="card card-detail border-primary mb-3">
         <div className="card-header">{title}</div>
         <div className="card-body text-primary">
           <p className="card-text">{description}</p>
