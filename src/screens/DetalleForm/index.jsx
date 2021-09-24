f0c8735b6170c5630bad9095d31fe9fc34ce6a29
Gonzalo <gon.pineiro@gmail.com>
2021-09-24 11:33:46 -0300

    :hammer: Table field

--- src/screens/DetalleForm/index.jsx
+++ src/screens/DetalleForm/index.jsx
@@ -19,9 +19,38 @@ const DetalleForm = () => {
   if (!form) return <Loading />;
 
   return (
-    <div className="container">
-      <h2 className="titulo text-center">DetalleForm</h2>
-      <div className="row mt-5"></div>
+    <div className="container pt-5">
+      <h2 className="titulo text-center">{form.name}</h2>
+      <div className="row mt-5">
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Etiqueta</th>
+              <th scope="col">Descripción</th>
+              <th scope="col">Titulo</th>
+              <th scope="col">Tipo</th>
+              <th scope="col">Requerido</th>
+              <th scope="col">Min</th>
+              <th scope="col">Max</th>
+              <th scope="col">Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {form.fields.map((field, key) => (
+              <tr key={key}>
+                <td>{field.field_label || "-"}</td>
+                <td>{field.separator_description || "-"}</td>
+                <td>{field.separator_title || "-"}</td>
+                <td>{field.field_type || "-"}</td>
+                <td>{field.field_required === true ? "Si" : "No" || "-"}</td>
+                <td>{field.min_lenght || "-"}</td>
+                <td>{field.max_lenght || "-"}</td>
+                <td>{field.field_options ? "Si" : "-" || "-"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
