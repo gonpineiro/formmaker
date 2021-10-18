2cbdafdc79e939f963f1a7bb6bc3ed35c55731dc
Gonzalo <gon.pineiro@gmail.com>
2021-10-18 08:33:19 -0300

    :hammer: Cambio Title MenuForm

--- src/screens/MenuForm/index.jsx
+++ src/screens/MenuForm/index.jsx
@@ -25,7 +25,7 @@ const MenuForm = () => {
 
   return (
     <div className="container pt-5">
-      <h2 className="titulo text-center">Listado de Formularios</h2>
+      <h2 className="titulo text-center">Hacé click e inscribite</h2>
       <div className="row pt-5">
         <div className="col-12 col-md-7 mx-auto">
           {forms.map((form, key) => (
