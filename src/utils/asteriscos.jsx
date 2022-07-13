acbed13ad37870c46f686f1434cfb365c166cc9e
fbastidas <96058026+fbastidas@users.noreply.github.com>
2022-07-13 14:50:03 -0300

    Update asteriscos.jsx
    MINOR FIX
    
    Nueva validacion para mostrar los asteriscos en el preview. Se valida con el boolean true ya que dicho valor sale directo del formulario en vista previa y no del json

--- src/utils/asteriscos.jsx
+++ src/utils/asteriscos.jsx
@@ -1,5 +1,5 @@
 const asteriscos = (field_required) => {
-  if (field_required === "true") {
+  if (field_required === "true" || field_required === true) {
     return <span className="text-danger fw-bold"> *</span>;
   }
 };
