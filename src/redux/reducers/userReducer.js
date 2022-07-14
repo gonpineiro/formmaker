4614a04d32031cfeff7181a05bdfe87f3884d49b
fbastidas <96058026+fbastidas@users.noreply.github.com>
2022-07-14 13:31:29 -0300

    Update userReducer.js
    Modificacion del redux para recuperar el dni del usuario, utilizado al momento de recuperar los formularios.

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -6,11 +6,13 @@ const INITIAL_STATE = {
   loading: true,
   error: null,
   isAdmin: "",
+  dniLoggedUser: null,
   idForm: undefined,
 };
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = INITIAL_STATE, action) => {
+  //console.log("Action: "+JSON.stringify(action));
   const payload = action.payload;
 
   switch (action.type) {
@@ -24,12 +26,18 @@ export default (state = INITIAL_STATE, action) => {
       }
 
       const datosPersonales = payload.datosPersonales;
-      /* const app = datosPersonales.apps.filter((obj) => obj.id === APP_ID)[0]; */
+
+      //console.log("data: "+JSON.stringify(datosPersonales));
+      //console.log("data2: "+JSON.stringify(datosPersonales.datosPersonales));
+      
+      const app = datosPersonales.apps != undefined ? datosPersonales.apps.filter((obj) => obj.id === APP_ID)[0] : null;
       return {
         ...state,
         loading: false,
         datosPersonales,
-        isAdmin: /* app && app.userProfiles === "3" ? */ true/*  : false */,
+        //isAdmin: /* app && app.userProfiles === "3" ? */ true/*  : false */,
+        isAdmin: (app != null ? app.userProfiles : false),
+        dniLoggedUser: (datosPersonales.datosPersonales != null &&  datosPersonales.datosPersonales != undefined ? datosPersonales.datosPersonales.documento : null),
         idForm: payload.idForm,
       };
 
