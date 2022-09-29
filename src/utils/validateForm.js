8e79801606e8c5bea9f9a04148651ddd5e9676aa
fbastidas <96058026+fbastidas@users.noreply.github.com>
2022-09-29 09:54:18 -0300

    Update validateForm.js
    Minor fix:
    - Se agrega la validacion de campo obligatorio a los input checkboxes y radio, y se verifica que se condiga con el valor el valor en texto o primitivo

--- src/utils/validateForm.js
+++ src/utils/validateForm.js
@@ -16,16 +16,19 @@ const validateForm = (fields) => {
         break;
       case "checkboxes":
       case "radio":
-        const radio = document.getElementsByName(req.field_name);
-        let countRadio = 0;
-        radio.forEach((radio) => {
-          if (radio.checked) countRadio++;
-        });
-        if (countRadio === 0) {
-          sendPost = false;
-          className("id" + req.field_id, "label-invalid", "add");
-        } else {
-          className("id" + req.field_id, "label-invalid", "remove");
+        if(req.field_required === true || req.field_required === "true"){
+          //console.log("required? "+ req.field_required);
+          const radio = document.getElementsByName(req.field_name);
+          let countRadio = 0;
+          radio.forEach((radio) => {
+            if (radio.checked) countRadio++;
+          });
+          if (countRadio === 0) {
+            sendPost = false;
+            className("id" + req.field_id, "label-invalid", "add");
+          } else {
+            className("id" + req.field_id, "label-invalid", "remove");
+          }
         }
 
         break;
@@ -34,12 +37,12 @@ const validateForm = (fields) => {
         break;
 
       default:
-        if (req.field_value === "" && req.field_required === "true") {
+        if (req.field_value === "" && (req.field_required === "true" || req.field_required === true) ) {
           className("id" + req.field_id, "is-invalid", "add");
           className("id" + req.field_id, "is-valid", "remove");
           sendPost = false;
         } else {
-          if (req.field_value !== "" && req.field_required === "true") {
+          if (req.field_value !== "" && (req.field_required === "true" || req.field_required === true)) {
             className("id" + req.field_id, "is-invalid", "remove");
             className("id" + req.field_id, "is-valid", "add");
           }
