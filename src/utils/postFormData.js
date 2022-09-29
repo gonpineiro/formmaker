911cd3413833b1cf9da95a6a4dcc87b00034e734
fbastidas <96058026+fbastidas@users.noreply.github.com>
2022-09-29 09:46:17 -0300

    Create postFormData.js
    un postformdata que no se de donde salio ¿?

---
+++ src/utils/postFormData.js
@@ -0,0 +1,19 @@
+import qs from "qs";
+import { TOKEN, URL_BACK } from "../config/config";
+
+const postData = async (data, nombre, type) => {
+  data.token = TOKEN;
+  data.type = type;
+  data.nombre = nombre;
+  const req = await fetch(URL_BACK, {
+    method: "POST",
+    body: qs.stringify(data),
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    },
+  });
+  const res = await req.json();
+  return res;
+};
+
+export default postData;
