447088ee48eb1e20ca394ddf5d950a9014b91c05
fbastidas <96058026+fbastidas@users.noreply.github.com>
2023-01-30 23:27:49 -0300

    Update createFormData.js
    Se modifica la logica de la creacion del formdata para adaptarse a los campos dependientes.
    
    Como es posible que algunos campos nunca sean renderizados, se comprueba que el campo haya sido seleccionado y de ser asi, se registra su valor en el formdata. Caso contrario, simplemente se agrega con valor "-"

--- src/utils/createFormData.js
+++ src/utils/createFormData.js
@@ -2,27 +2,38 @@ const createFormData = (form, fields) => {
   const Formdata = new FormData(form);
   let inputValue;
   fields.forEach((req) => {
+    // console.group();
+    // console.log(req);
+    // console.groupEnd();
     if (req.field_name) {
       switch (req.field_type) {
         case "radio":
           const radio = document.getElementsByName(req.field_name);
-          radio.forEach((radio) => {
-            if (radio.checked) {
-              const content = radio.nextSibling.textContent;
-              Formdata.set(req.field_name, content);
-            }
-          });
+          if(radio.length > 0){
+            radio.forEach((radio) => {
+              if (radio.checked) {
+                const content = radio.nextSibling.textContent;
+                Formdata.set(req.field_name, content);
+              }
+            });
+          }else{
+            Formdata.set(req.field_name, "-");
+          }
           break;
 
         case "checkboxes":
           const checkbox = document.getElementsByName(req.field_name);
-          let values = [];
-          checkbox.forEach((item) => {
-            if (item.checked) {
-              values.push(item.value);
-            }
-          });
-          Formdata.set(req.field_name, values);
+          if(radio.length > 0){
+            let values = [];
+            checkbox.forEach((item) => {
+              if (item.checked) {
+                values.push(item.value);
+              }
+            });
+            Formdata.set(req.field_name, values);
+          }else{
+            Formdata.set(req.field_name, "-");
+          }
           break;
 
         case "file":
@@ -36,9 +47,9 @@ const createFormData = (form, fields) => {
             //}
           });*/
           //console.log("Campo file: "+document.getElementsByName(req.field_name));
-          console.log("Campo file1: "+document.getElementById(req.field_id));
-          console.log("Campo file2: "+document.getElementById(req.field_id).files);
-          console.log("Campo file3: "+JSON.stringify(document.getElementById(req.field_id)));
+          // console.log("Campo file1: "+document.getElementById(req.field_id));
+          // console.log("Campo file2: "+document.getElementById(req.field_id).files);
+          // console.log("Campo file3: "+JSON.stringify(document.getElementById(req.field_id)));
           Formdata.append("files[]", document.getElementsByName(req.field_name).files[0]);
           //Formdata.set("files[]", files);
           break;
@@ -49,21 +60,41 @@ const createFormData = (form, fields) => {
           //inputValue = Formdata.get(req.field_name);
           inputValue = document.getElementsByName(req.field_name);
           //console.log("\nInput:"+inputValue);
-          if(req.field_id !== 'acepto' && (inputValue[0].value == undefined || inputValue[0].value == null || inputValue[0].value == 'null')){
-            Formdata.set(req.field_name, inputValue[0].value);
-          }else{
-            console.log("\nInput:"+inputValue[0]);
+          // if(req.field_id !== 'acepto' && (inputValue[0].value == undefined || inputValue[0].value == null || inputValue[0].value == 'null')){
+          //   Formdata.set(req.field_name, inputValue[0].value);
+          // }else{
+          //   console.log("\nInput:"+inputValue[0]);
+          // }
+          if(req.field_id !== 'acepto'){
+            console.log(req.field_name);
+            console.log(inputValue[0]);
+            // Formdata.set(req.field_name, ((inputValue[0].value == undefined || inputValue[0].value == null || inputValue[0].value == 'null') ? "-" : inputValue[0].value));
+            if([undefined, null, 'null', ''].includes(inputValue[0])){
+              Formdata.set(req.field_name, "-");
+            }else if([undefined, null, 'null', ''].includes(inputValue[0].value)){
+              Formdata.set(req.field_name, "-");
+            }else{
+              Formdata.set(req.field_name, inputValue[0].value);
+            }
           }
+          // else{
+          //   console.log("\nInput:"+inputValue[0]);
+          // }
           break;
       }
     }
   });
   let formObject = {};
 
+  // for (const value of Formdata.values()) {
+  //   console.log(value);
+  // }
+
   for (const key of Formdata.keys()) {
     //se agrega esta condicion para probar si el problema de los campos en nulo proviene del front.
     //La idea es que si se devuelve un form data nulo, se le indique al usuario que no se pudo registrar
     //su respuesta. El formdata nulo surge si alguno de los valores obtenidos es nulo
+
     if(key != "acepto"){
       if(formObject != null){
         if(Formdata.get(key) != null && Formdata.get(key) != 'null' && Formdata.get(key) != undefined ){
@@ -77,7 +108,7 @@ const createFormData = (form, fields) => {
   if(formObject != null){
     delete formObject.acepto;
   }
-  //formObject = null; //utilizado para probar el caso nulo
+  // formObject = null; //utilizado para probar el caso nulo
 
   return formObject;
 };
